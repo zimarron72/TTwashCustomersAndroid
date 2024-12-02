ca434d56fe0af8a07161598398593cd83086817d
Jorman Sandoval <jormansandoval@Mac.lan>
2024-12-02 18:45:35 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ttwash customers ',
+  webDir: 'www'
+};
+
+export default config;
