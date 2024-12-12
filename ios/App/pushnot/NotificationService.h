de8ef869f14a91bd4cdf7984c5ccc07085eb7da3
zimarron72 <90347689+zimarron72@users.noreply.github.com>
2024-12-12 18:31:47 -0500

    Angular Fire instalado con codigo de login sin dar resultados en las pruebas, aparentemente apple login funcionado.

---
+++ ios/App/pushnot/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  pushnot
+//
+//  Created by Jorman Sandoval on 12/8/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
